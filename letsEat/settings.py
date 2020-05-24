62810e02e1cc4dba6f37535aa1bbbc9d831c0ebf
Lily Sai <sai.lily01@gmail.com>
2020-05-23 23:18:17 -0700

    changed templates in settings.py

--- letsEat/settings.py
+++ letsEat/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'letsEat.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': ['templates', os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
